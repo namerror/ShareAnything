d4db92d8002a09455323e7a2dd8d28cf61f3e572
namerror <44743690+namerror@users.noreply.github.com>
2024-03-10 16:04:42 +0100

    Update settings.py
    Fixing whitenoise server error 500

--- website/settings.py
+++ website/settings.py
@@ -129,7 +129,7 @@ if DEBUG:
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
