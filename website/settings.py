62b1e4650ff60dc045a72c0aaa46e3d55446aed1
namerror <44743690+namerror@users.noreply.github.com>
2024-03-10 15:14:31 +0100

    Update settings.py
    static files

--- website/settings.py
+++ website/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
